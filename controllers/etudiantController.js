33aed911be66b3f7f45c5d98fb90bc8f58941430
malekannabi <60436343+malek-annabi@users.noreply.github.com>
2022-08-18 15:47:53 +0100

    student management

--- controllers/etudiantController.js
+++ controllers/etudiantController.js
@@ -1,23 +1,101 @@
 // add Student
 exports.addStudent = (req, res, next) => {
-	if (!req.body) return next(new AppError("No form data found", 404));
-	let nom = req.body.nom;
-	let prenom = req.body.prenom;
-	let nationalite = req.body.nationalite;
-	let tel = req.body.tel;
-	let dateNaissance = req.body.dateNaissance;
-	let lieuNaissance = req.body.enterpriseName;
-	let cin = req.body.cin;
-	// add in cockpit database
-	con.query(
-	  "INSERT INTO etudiant (nom,prenom,tel,nationalite,dateNaissance,lieuNaissance,CIN) VALUES (?,?,?,?,?,?,?);",
-	  [nom, prenom, tel, nationalite, dateNaissance, lieuNaissance, cin],
-	  function (err, data, fields) {
-		if (err) throw err;
-		res.status(201).json({
-		  status: "success",
-		  message: "etudiant ajoute!",
-		});
-	  }
-	);
-  };
+  if (!req.body) return next(new AppError("No form data found", 404));
+  let nom = req.body.nom;
+  let prenom = req.body.prenom;
+  let nationalite = req.body.nationalite;
+  let tel = req.body.tel;
+  let dateNaissance = req.body.dateNaissance;
+  let lieuNaissance = req.body.lieuNaissance;
+  let cin = req.body.cin;
+  // add in cockpit database
+  con
+    .promise()
+    .query(
+      "INSERT INTO etudiant (nom,prenom,tel,nationalite,dateNaissance,lieuNaissance,CIN) VALUES (?,?,?,?,?,?,?);",
+      [nom, prenom, tel, nationalite, dateNaissance, lieuNaissance, cin]
+    )
+    .then(([rows, fields]) => {
+      res.status(201).json({
+        status: "success",
+        message: "etudiant ajoute!",
+      });
+    })
+    .catch((err) =>
+      res
+        .status(400)
+        .json({
+          status: "bad request",
+          message: "etudiant non ajoute!",
+          err: err.message,
+        })
+    )
+    .then(() => con.end());
+};
+exports.updateStudent = (req, res, next) => {
+  if (!req.body) return next(res.send("No form data found", 404));
+  let nom = req.body.nom;
+  let prenom = req.body.prenom;
+  let nationalite = req.body.nationalite;
+  let tel = req.body.tel;
+  let dateNaissance = req.body.dateNaissance;
+  let lieuNaissance = req.body.lieuNaissance;
+  let cin = req.body.cin;
+  let idEtudiant = req.body.idEtudiant;
+  // add in cockpit database
+  con
+    .promise()
+    .query(
+      "UPDATE etudiant SET nom=?,prenom=?,tel=?,nationalite=?,dateNaissance=?,lieuNaissance=?,CIN=? WHERE idEtudiant=?;",
+      [
+        nom,
+        prenom,
+        tel,
+        nationalite,
+        dateNaissance,
+        lieuNaissance,
+        cin,
+        idEtudiant,
+      ]
+    )
+    .then(([rows, fields]) => {
+      res.status(200).json({
+        status: "success",
+        message: "etudiant a ete mis a jour!",
+      });
+    })
+    .catch((err) =>
+      res
+        .status(400)
+        .json({
+          status: "bad request",
+          message: "etudiant non modifier!",
+          err: err.message,
+        })
+    )
+    .then(() => con.end());
+};
+
+exports.deleteStudent = (req, res, next) => {
+  if (!req.body) return next(res.send("No form data found", 404));
+  let idEtudiant = req.body.idEtudiant;
+  con
+    .promise()
+    .query("DELETE FROM etudiant WHERE idEtudiant=?;", [idEtudiant])
+    .then(([rows, fields]) => {
+      res.status(200).json({
+        status: "success",
+        message: "etudiant a ete suprime!",
+      });
+    })
+    .catch((err) =>
+      res
+        .status(400)
+        .json({
+          status: "bad request",
+          message: "etudiant non suprime!",
+          err: err.message,
+        })
+    )
+    .then(() => con.end());
+};
--- routes/student.js
+++ routes/student.js
@@ -2,5 +2,7 @@ const etudiantController=require('../controllers/etudiantController.js');
 
 // auth rooute
 module.exports=(app)=>{
-    app.post('/add/etudaint',etudiantController.addStudent);
+    app.post('/add/etudiant',etudiantController.addStudent);
+    app.post('/update/etudiant',etudiantController.updateStudent);
+    app.post('/delete/etudiant',etudiantController.deleteStudent);
 }
