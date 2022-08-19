618d27f89c7d97a5005091167ed2e3477cb73f14
malekannabi <60436343+malek-annabi@users.noreply.github.com>
2022-08-19 06:01:45 +0100

    get students

--- controllers/etudiantController.js
+++ controllers/etudiantController.js
@@ -30,7 +30,6 @@ exports.addStudent = (req, res, next) => {
           err: err.message,
         })
     )
-    .then(() => con.end());
 };
 exports.updateStudent = (req, res, next) => {
   if (!req.body) return next(res.send("No form data found", 404));
@@ -73,7 +72,6 @@ exports.updateStudent = (req, res, next) => {
           err: err.message,
         })
     )
-    .then(() => con.end());
 };
 
 exports.deleteStudent = (req, res, next) => {
@@ -97,5 +95,29 @@ exports.deleteStudent = (req, res, next) => {
           err: err.message,
         })
     )
-    .then(() => con.end());
+};
+exports.getStudents = (req, res, next) => {
+  if (!req.body) return next(new AppError("No form data found", 404));
+  con
+    .promise()
+    .query(
+      "SELECT * FROM etudiant;"
+    )
+    .then(([rows]) => {
+      res.status(201).json({
+        status: "success",
+        message: "tout les etudiants!",
+        etudiants: rows
+      });
+    })
+    .catch((err) =>
+      res
+        .status(400)
+        .json({
+          status: "bad request",
+          message: "etudiant non ajoute!",
+          err: err.message,
+        })
+    )
+    
 };
--- routes/student.js
+++ routes/student.js
@@ -5,4 +5,5 @@ module.exports=(app)=>{
     app.post('/add/etudiant',etudiantController.addStudent);
     app.post('/update/etudiant',etudiantController.updateStudent);
     app.post('/delete/etudiant',etudiantController.deleteStudent);
+    app.get('/get/etudiants',etudiantController.getStudents);
 }
